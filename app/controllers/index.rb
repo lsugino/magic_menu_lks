7e6d60abb7363b3bd5cb2323a6bfc51426dff38e
lindseysugino <lindseysugino@gmail.com>
2013-11-17 16:38:32 -0800

    updates

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,3 @@
-# get '/' do
-# 	@menus = Menu.all
-#   erb :index
-# end
-
-# post '/' do
-# 	Menu.create(name: params[:menu_name])
-#   erb :index
-# end
-
-#################################
 
 get '/' do
   erb :index
@@ -44,43 +33,3 @@ post '/item/new' do
   return item.name if item
   return 'false'
 end        
-
-
-
-
-### - TEST - #############################
-
-# get '/' do
-#   erb :index
-# end
-
-# get '/menu/:id' do
-#   @menu = Menu.find_by_id(params[:id])
-#   session[:current_menu] = @menu
-#   erb :menu
-#   # at some point, only the menus a user has permission to see should be shown
-# end
-
-# post '/menu/new' do
-#   menu = Menu.create name: params[:menuTitle] unless params[:menuTitle].empty?
-#   return menu.name if menu
-#   return 'false'
-# end        
-
-# get '/item/:id' do
-#   @item = Item.find_by_id(params[:id]) 
-#   @items_menus = Mitem.where(item_id: params[:id])
-#   erb :item
-#   # at some point, only the items a user has permission to see should be shown
-# end
-
-# post '/item/new' do
-#   item = Item.create name: params[:itemTitle] unless params[:itemTitle].empty?
-#   unless params[:itemTitle].empty? 
-#           params[:menu].each do |menu_id|
-#     Mitem.create item_id: item.id, menu_id: menu_id.to_i unless params[:menu].empty?
-#           end
-#   end          
-#   return item.name if item
-#   return 'false'
-# end        
